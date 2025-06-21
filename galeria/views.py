e270ed7efe8d3d501606df0445da0c067da7f0ed
Daniel Solis Salge <danisalgedev@gmail.com>
2025-06-21 08:18:07 -0300

    Enhance search functionality in gallery view and update buscar.html template for improved layout and user experience

--- galeria/views.py
+++ galeria/views.py
@@ -10,4 +10,11 @@ def image(request, foto_id):
     return render(request, 'galeria/image.html', {"fotografia": fotografia})
 
 def buscar(request):
-    return render(request, 'galeria/buscar.html')
+    fotografias = Fotografia.objects.filter(publicada=True).order_by("data_fotografia")
+    
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET["buscar"]
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+    
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})
